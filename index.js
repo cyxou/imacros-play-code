17ee0bd57203a0c922dea123ba8660fea6565f01
cyxou <acyxou@gmail.com>
2015-10-06 15:32:40 +0300

    Initial version

---
+++ index.js
@@ -0,0 +1,49 @@
+/* global iimPlayCode, iimGetLastExtract, iimGetLastError, window, module*/
+
+/**
+ * A wrapper around iimPlayCode function that allows a user to terminate script execution
+ * when pressing the STOP button.
+ *
+ * @param  {string} macro Macro to play in iMacros' language.
+ * @param  {object} opts  Optional parameters object that includes:
+ *                        errorIgnore {boolean} if true than iMacros won't stop on errors uccured
+ *                        timeoutStep {integer} timeout to search for a TAG on a webpage before failing with error
+ * @return {object}       Object with two properties:
+ *                        code - execution code returned by iMacros' iimPlayCode() function
+ *                        extract - extracted data if any;
+ */
+module.exports = function (macro, opts) { "use strict";
+	opts = opts || {};
+	var errorIgnore = opts["errorIgnore"] ? "YES" : "NO" || "NO";
+	var timeoutStep = opts["timeoutStep"] || 1;
+
+  macro = "SET !REPLAYSPEED FAST" +
+		"\nSET !ERRORIGNORE " + errorIgnore +
+		"\nSET !TIMEOUT_STEP " + timeoutStep +
+		"\n" + macro;
+
+  var code = iimPlayCode(macro);
+
+	switch (code) {
+		case -101:
+			throw new Error("\nРабота скрипта прервана пользователем.\n");
+		case -920:
+		case -921:
+		case -922:
+		case -923:
+		case -924:
+		case -925:
+		case -926:
+			window.console.error("Элемент не найден на текущей странице.", iimGetLastError());
+			break;
+		case -1001:
+			window.console.error("Ошибка при выполнении действия на странице.", iimGetLastError());
+	}
+
+	var extract = iimGetLastExtract().split("[EXTRACT]");
+
+	return {
+		"code": code,
+		"extract": extract.length === 1 ? extract[0] : extract
+	};
+}
